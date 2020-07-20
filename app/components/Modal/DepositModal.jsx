f1db2c6caa857648384b41833d623a191f14d930
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-07-21 02:41:34 +0300

    Update DepositModal.jsx

--- app/components/Modal/DepositModal.jsx
+++ app/components/Modal/DepositModal.jsx
@@ -1,4 +1,4 @@
-import React, {Component, Children} from "react";
+import React from "react";
 import Translate from "react-translate-component";
 import utils from "common/utils";
 import {requestDepositAddress} from "common/gatewayMethods";
@@ -20,59 +20,6 @@ import {getGatewayStatusByAsset} from "common/gatewayUtils";
 import CryptoLinkFormatter from "../Utility/CryptoLinkFormatter";
 import counterpart from "counterpart";
 import {Modal, Button} from "bitshares-ui-style-guide";
-import {connect} from "alt-react";
-import ChainTypes from "components/Utility/ChainTypes";
-import BindToChainState from "components/Utility/BindToChainState";
-import AccountStore from "stores/AccountStore";
-import {toPairs} from "lodash-es";
-
-class ResolvemyActiveAccountsChainState extends Component {
-    static propTypes = {
-        myActiveAccounts: ChainTypes.ChainAccountsList.isRequired
-    };
-
-    render() {
-        let myActiveAccounts = [];
-        toPairs(this.props.myActiveAccounts).forEach(account => {
-            if (!account[1]) return;
-            console.log("... account.toJS()", account[1].toJS());
-            myActiveAccounts.push(account[1]);
-        });
-        let child = Children.only(this.props.children);
-        if (!child)
-            return (
-                <span>{myActiveAccounts.map(a => <br>{a.toJS()}</br>)}</span>
-            );
-        // Pass the list to a child reactjs component as this.props.resolvedmyActiveAccounts
-        child = React.cloneElement(child, {myActiveAccounts});
-        return <span>{child}</span>;
-    }
-}
-ResolvemyActiveAccountsChainState = BindToChainState(
-    ResolvemyActiveAccountsChainState
-);
-
-class ResolvemyActiveAccounts extends Component {
-    render() {
-        return (
-            <ResolvemyActiveAccountsChainState
-                myActiveAccounts={this.props.myActiveAccounts}
-                children={this.props.children}
-            />
-        );
-    }
-}
-
-ResolvemyActiveAccounts = connect(ResolvemyActiveAccounts, {
-    listenTo() {
-        return [AccountStore];
-    },
-    getProps() {
-        return AccountStore.getState();
-    }
-});
-
-export default ResolvemyActiveAccounts;
 
 class DepositModalContent extends DecimalChecker {
     constructor() {
