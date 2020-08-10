34563806e5ebc8a72408ff4f113613118124a1c3
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-08-10 11:49:01 +0300

    Update TicketCreate.jsx

--- app/components/Blockchain/operations/TicketCreate.jsx
+++ app/components/Blockchain/operations/TicketCreate.jsx
@@ -7,7 +7,6 @@ import {ChainTypes as grapheneChainTypes} from "bitsharesjs";
 
 export const TicketCreate = ({op, linkToAccount, fromComponent}) => {
    const {operations} = grapheneChainTypes;
-   let op = Object.keys(operations);
    const ticket_type = Object.keys(
                     grapheneChainTypes.ticket_type
                 ).find(
