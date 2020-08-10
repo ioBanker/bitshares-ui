2380f525a431c72855050cf2cb5e8f7535c0e686
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-08-10 10:02:34 +0300

    Update TicketCreate.jsx

--- app/components/Blockchain/operations/TicketCreate.jsx
+++ app/components/Blockchain/operations/TicketCreate.jsx
@@ -3,34 +3,31 @@ import Translate from "react-translate-component";
 import FormattedAsset from "../../Utility/FormattedAsset";
 import TranslateWithLinks from "../../Utility/TranslateWithLinks";
 import counterpart from "counterpart";
-import {ChainTypes as grapheneChainTypes, ChainStore} from "bitsharesjs";
+import {ChainTypes} from "bitsharesjs";
 
-const {operations} = grapheneChainTypes;
-let ops = Object.keys(operations);
-export const ticket_type = Object.keys(
-                    grapheneChainTypes.ticket_type
-                ).find(
-                    key =>
-                        grapheneChainTypes.ticket_type[key] ===
-                        op[1].target_type
-                );
+export const TicketCreate = ({op, linkToAccount, fromComponent}) => {
+    const ticket_type = Object.keys(ChainTypes.ticket_type).find(
+        key => ChainTypes.ticket_type[key] === op[1].target_type
+    );
     return (
-<span>
-                        <TranslateWithLinks
-                            string="operation.ticket_create"
-                            keys={[
-                                {
-                                    type: "account",
-                                    value: op[1].account,
-                                    arg: "account"
-                                },
-                                {
-                                    type: "amount",
-                                    value: op[1].amount,
-                                    arg: "amount"
-                                }
-                            ]}
-                        /> &nbsp; ({counterpart.translate("operation.ticket_types." + ticket_type)})
-</span>
+        <span>
+            <TranslateWithLinks
+                string="operation.ticket_create"
+                keys={[
+                    {
+                        type: "account",
+                        value: op[1].account,
+                        arg: "from"
+                    },
+                    {
+                        type: "amount",
+                        value: op[1].amount,
+                        arg: "amount"
+                    }
+                ]}
+            />
+            &nbsp; (
+            {counterpart.translate("operation.ticket_types." + ticket_type)})
+        </span>
     );
 };
