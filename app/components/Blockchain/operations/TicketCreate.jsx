052f3d31eaee81e5f702067e660c1e03d12a2c61
ioBanker <37595908+ioBanker@users.noreply.github.com>
2020-08-10 09:39:09 +0300

    Update TicketCreate.jsx

--- app/components/Blockchain/operations/TicketCreate.jsx
+++ app/components/Blockchain/operations/TicketCreate.jsx
@@ -5,29 +5,32 @@ import TranslateWithLinks from "../../Utility/TranslateWithLinks";
 import counterpart from "counterpart";
 import {ChainTypes as grapheneChainTypes, ChainStore} from "bitsharesjs";
 
-export const TicketCreate = ({op, linkToAccount, fromComponent}) => {
-    const ticket_type = Object.keys(ChainTypes.ticket_type).find(
-        key => ChainTypes.ticket_type[key] === op[1].target_type
-    );
+const {operations} = grapheneChainTypes;
+let ops = Object.keys(operations);
+export const ticket_type = Object.keys(
+                    grapheneChainTypes.ticket_type
+                ).find(
+                    key =>
+                        grapheneChainTypes.ticket_type[key] ===
+                        op[1].target_type
+                );
     return (
-        <span>
-            <TranslateWithLinks
-                string="operation.ticket_create"
-                keys={[
-                    {
-                        type: "account",
-                        value: op[1].account,
-                        arg: "from"
-                    },
-                    {
-                        type: "amount",
-                        value: op[1].amount,
-                        arg: "amount"
-                    }
-                ]}
-            />
-            &nbsp; (
-            {counterpart.translate("operation.ticket_types." + ticket_type)})
-        </span>
+<span>
+                        <TranslateWithLinks
+                            string="operation.ticket_create"
+                            keys={[
+                                {
+                                    type: "account",
+                                    value: op[1].account,
+                                    arg: "account"
+                                },
+                                {
+                                    type: "amount",
+                                    value: op[1].amount,
+                                    arg: "amount"
+                                }
+                            ]}
+                        /> &nbsp; ({counterpart.translate("operation.ticket_types." + ticket_type)})
+</span>
     );
 };
